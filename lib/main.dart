87005f3f1ff7c7cdffca50ca6254ceb168937281
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2024-10-13 18:52:25 +0700

    theme app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/feartures/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Blog App',
+      theme: AppTheme.drakThemeMode,
       home: const SignupPage(),
     );
   }
